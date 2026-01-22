f5da30ad8c4787bd1d170d6e90a4d0cd2c9f4050
pheladimothiba-beep <pheladi.mothiba@ul.ac.za>
2026-01-22 15:47:59 +0200

    Implement calculator menu with multiple functions
    program added

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -1,45 +1,105 @@
-﻿namespace ConsoleApp8
+CO
+using System;
+
+namespace ConsoleApp20
 {
-    internal class Calculator
+    class Program
     {
-        // Constructor
-        public Calculator()
+        static void Main(string[] args)
         {
+            Calculator calc = new Calculator();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\n==== CALCULATOR MENU ====");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Subtract");
+                Console.WriteLine("3. Multiply");
+                Console.WriteLine("4. Divide");
+                Console.WriteLine("5. Square Root");
+                Console.WriteLine("6. Power");
+                Console.WriteLine("7. Logarithm");
+                Console.WriteLine("8. Sine");
+                Console.WriteLine("9. Cosine");
+                Console.WriteLine("10.Tangent");
+                Console.WriteLine("11.Factorial");
+                Console.WriteLine("12.Absolute Value");
+                Console.WriteLine("13.Percentage");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        calc.AddNumbers();
+                        break;
+
+                    case "2":
+                        calc.SubtractNumbers();
+                        break;
+
+                    case "3":
+                        calc.MultiplyNumbers();
+                        break;
+
+                    case "4":
+                        calc.DivideNumbers();
+                        break;
+
+                    case "5":
+                        calc.SquareRoot();
+                        break;
+
+                    case "6":
+                        calc.Power();
+                        break;
+
+                    case "7":
+                        calc.Logarithm();
+                        break;
+
+                    case "8":
+                        calc.Sine();
+                        break;
+
+                    case "9":
+                        calc.Cosine();
+                        break;
+
+                    case "10":
+                        calc.Tangent();
+                        break;
+
+                    case "11":
+                        calc.Factorial();
+                        break;
+
+                    case "12":
+                        calc.AbsoluteValue();
+                        break;
+
+                    case "13":
+                        calc.Percentage();
+                        break;
+
+                    case "0":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                }
+                Console.WriteLine("Thank you for using the calculator. Goodbye!");
+                Console.ReadLine();
+            }
         }
+    }
+}
 
-        public void AddNumbers()
-        { 
-            Console.WriteLine("enter the first number");
-            double num1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter the second number");
-            double num2 = double.Parse(Console.ReadLine());
-
-            double result = num1 + num2;
-            Console.WriteLine($" Result: {result}");
-        }
-
-        public void SubtractNumbers()
-        {
-            Console.WriteLine("enter the first number ");
-            double num1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("enter the second number");
-            double num2 = double.Parse(Console.ReadLine());
-
-            double sum = num1 - num2;
-            Console.WriteLine("the sum is :" + sum);
-        }
-
-        
-
-       
-        }
-
-        
-        
-
-        
-        }
-    
 
