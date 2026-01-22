6d77cae51a2200599b5707270ec191c00a453627
Ngoato-UL <ngoato.mahango@ul.ac.za>
2026-01-22 14:38:48 +0200

    done a bit of the Program side

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp8
+{
+    internal class Calculator
+    {
+        // Constructor
+        public Calculator()
+        {
+        }
+
+        public void AddNumbers()
+        { 
+            Console.WriteLine("enter the first number");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = num1 + num2;
+            Console.WriteLine($" Result: {result}");
+        }
+
+        public void SubtractNumbers()
+        {
+            Console.WriteLine("enter the first number ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double sum = num1 - num2;
+            Console.WriteLine("the sum is :" + sum);
+        }
+
+        
+
+       
+        }
+
+        
+        
+
+        
+        }
+    
+
